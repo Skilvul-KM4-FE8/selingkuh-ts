70df17e9a69c503f22a5b042529cafc40b5716cb
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-05-20 07:45:45 +0700

    fixing landing

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,28 +1,30 @@
+import { Button } from "../ui/button";
+import Logo from "../../../public/logoipsum-223.svg";
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <div>
-      <nav className="fixed top-0 left-0 w-full flex items-center justify-between p-4 bg-white shadow-md dark:bg-zinc-800/50">
+      <nav className="fixed top-0 left-0 w-full flex items-center justify-between p-4 bg-transparant shadow-md dark:bg-zinc-800/50">
         <div className="flex items-center gap-4">
-          <a
-            href="/"
-            className="font-bold text-lg text-gray-800 dark:text-gray-100"
-          >
-            Selingkuh
-          </a>
-          <a href="/about" className="text-gray-600 dark:text-gray-400">
-            About
-          </a>
-          <a href="/contact" className="text-gray-600 dark:text-gray-400">
+          <Image src={Logo} alt="Selingkuh" className="w-10 h-10" />
+
+          <a href="/contact" className="text-white dark:text-gray-400">
             Contact
+          </a>
+          <a href="/contact" className="text-white dark:text-gray-400">
+            Chat
           </a>
         </div>
         <div className="flex items-center gap-4">
-          <a href="/login" className="text-gray-600 dark:text-gray-400">
+          <a href="/login" className="text-white dark:text-gray-400">
             Login
           </a>
-          <a href="/register" className="text-gray-600 dark:text-gray-400">
-            Register
-          </a>
+          <Button className="btn-selingkuh">
+            <a href="/register" className="text-white dark:text-gray-400">
+              Register
+            </a>
+          </Button>
         </div>
       </nav>
     </div>
