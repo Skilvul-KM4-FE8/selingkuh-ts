70ff17d7ccb38f97c62054486ebefe156353a473
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-05-21 21:58:49 +0700

    push register to alfito

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 import { registerUserInterface } from "@/app/interfaces/userInterfaces";
 
 export const registerUser = async (account: registerUserInterface) => {
-    const response = await fetch("/api/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: account.username,
-            email: account.email,
-            password: account.password, 
-        })
-    })
+  const response = await fetch("/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: account.username,
+      email: account.email,
+      password: account.password,
+    }),
+  });
 
-    const data: registerUserInterface = await response.json();
+  const data: registerUserInterface = await response.json();
 
-    return data
-}
+  return data;
+};
