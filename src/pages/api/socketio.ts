b1d670d726381a0da0543bcd464ebd24b2946fc3
Muhammad Rafai <rafaimhd123@gmail.com>
2024-06-12 14:58:00 +0700

    remove req.socket

--- src/pages/api/socketio.ts
+++ src/pages/api/socketio.ts
@@ -22,7 +22,7 @@ export default async function message(req: NextApiRequest, res: NextApiResponse)
     // console.log("================gt]-/==================")
     // console.log(res.socket.server.io)
     // console.log("================gt]-/==================")
-    if (req.socket && !(req as any).socket.server.io) {
+    if (!(req as any).socket.server.io) {
         console.log("Socket is initializing")
         const httpServer: SocketServer = (req as any).socket.server as SocketServer
         const io = new IOServer(httpServer, {
